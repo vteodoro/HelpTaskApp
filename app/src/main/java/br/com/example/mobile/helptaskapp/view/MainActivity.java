dd2904ea9dccd2b3e7c5cb799df0a10de2a29bda
vteodoro <viviteodoro19@gmail.com>
2018-04-24 23:58:38 -0300

    Organizar classes

--- app/src/main/java/br/com/example/mobile/helptaskapp/view/MainActivity.java
+++ app/src/main/java/br/com/example/mobile/helptaskapp/view/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.example.mobile.helptaskapp;
+package br.com.example.mobile.helptaskapp.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.example.mobile.helptaskapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
