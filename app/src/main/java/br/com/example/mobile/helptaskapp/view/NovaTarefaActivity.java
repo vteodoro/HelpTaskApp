12ff6543283c71739fbd2fd251e4878ce45f2f43
vteodoro <viviteodoro19@gmail.com>
2018-04-29 23:08:00 -0300

    Editar activity NovaTarefa

--- app/src/main/java/br/com/example/mobile/helptaskapp/view/NovaTarefaActivity.java
+++ app/src/main/java/br/com/example/mobile/helptaskapp/view/NovaTarefaActivity.java
@@ -1,6 +1,14 @@
 package br.com.example.mobile.helptaskapp.view;
 
-public class NovaTarefaActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-    
+import br.com.example.mobile.helptaskapp.R;
+
+public class NovaTarefaActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nova_tarefa);
+    }
 }
