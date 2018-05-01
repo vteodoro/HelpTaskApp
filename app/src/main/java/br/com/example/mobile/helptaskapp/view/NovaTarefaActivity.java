f8a41777fe19f5c8aad63675fef6dd5ef7510560
vteodoro <viviteodoro19@gmail.com>
2018-04-30 22:29:59 -0300

    Editar activity NovaTarefa - variaveis

--- app/src/main/java/br/com/example/mobile/helptaskapp/view/NovaTarefaActivity.java
+++ app/src/main/java/br/com/example/mobile/helptaskapp/view/NovaTarefaActivity.java
@@ -2,13 +2,29 @@ package br.com.example.mobile.helptaskapp.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import br.com.example.mobile.helptaskapp.R;
 
 public class NovaTarefaActivity extends AppCompatActivity{
+    private EditText titulo;
+    private EditText data;
+    private EditText hora;
+    private CheckBox urgente;
+    private CheckBox importante;
+    private EditText detalhes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nova_tarefa);
+
+        titulo = findViewById(R.id.etTitulo);
+        data = findViewById(R.id.etData);
+        hora = findViewById(R.id.etHora);
+        urgente = findViewById(R.id.ckUrgente);
+        importante = findViewById(R.id.ckImportante);
+        detalhes = findViewById(R.id.etDetalhes);
     }
 }
