2ef11771408bfaa813f16be5c080af373065a665
vteodoro <viviteodoro19@gmail.com>
2018-05-03 00:01:17 -0300

    Inserir acao aos itens de menu | Criar activity | Renomear ids

---
+++ app/src/main/java/br/com/example/mobile/helptaskapp/view/ListarTarefaActivity.java
@@ -0,0 +1,15 @@
+package br.com.example.mobile.helptaskapp.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.example.mobile.helptaskapp.R;
+
+public class ListarTarefaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_tarefas);
+    }
+}
--- app/src/main/java/br/com/example/mobile/helptaskapp/view/MainActivity.java
+++ app/src/main/java/br/com/example/mobile/helptaskapp/view/MainActivity.java
@@ -1,9 +1,11 @@
 package br.com.example.mobile.helptaskapp.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 import br.com.example.mobile.helptaskapp.R;
@@ -21,6 +23,20 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_tarefas, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+
+            case R.id.newTask:
+                startActivity(new Intent(this, NovaTarefaActivity.class));
+                return true;
+
+            case R.id.list:
+                startActivity(new Intent(this,ListarTarefaActivity.class ));
+        }
+        return onOptionsItemSelected(item);
     }
 
 
