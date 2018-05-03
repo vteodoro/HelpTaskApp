0d2fbadd4ab7562d2b99081f564b11ecab8082b9
vteodoro <viviteodoro19@gmail.com>
2018-05-02 23:36:06 -0300

    Editar MainActivity - MenuInflater

--- app/src/main/java/br/com/example/mobile/helptaskapp/view/MainActivity.java
+++ app/src/main/java/br/com/example/mobile/helptaskapp/view/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.example.mobile.helptaskapp.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
 
 import br.com.example.mobile.helptaskapp.R;
 
@@ -12,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_tarefas, menu);
+        return true;
+    }
+
+
 }
