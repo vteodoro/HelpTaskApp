931f29285216d539e4663bca78ed725486905b31
vteodoro <viviteodoro19@gmail.com>
2018-05-05 01:14:19 -0300

    Editar adapter e holder

--- app/src/main/java/br/com/example/mobile/helptaskapp/view/TarefaAdapter.java
+++ app/src/main/java/br/com/example/mobile/helptaskapp/view/TarefaAdapter.java
@@ -1,4 +1,43 @@
 package br.com.example.mobile.helptaskapp.view;
 
-public class TarefaAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import br.com.example.mobile.helptaskapp.R;
+import br.com.example.mobile.helptaskapp.model.Tarefa;
+
+public class TarefaAdapter extends RecyclerView.Adapter{
+
+    private List<Tarefa> tarefas;
+    private Context context;
+
+    public TarefaAdapter(List<Tarefa> tarefas, Context context){
+        this.tarefas = tarefas;
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(context).inflate(R.layout.lista_tarefas_item, parent, false);
+        TarefaViewHolder holder = new TarefaViewHolder(view, this);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        TarefaViewHolder viewHolder = (TarefaViewHolder) holder;
+
+        Tarefa tarefa = tarefas.get(position);
+        ((TarefaViewHolder)holder).preencher(tarefa);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tarefas.size();
+    }
 }
--- app/src/main/java/br/com/example/mobile/helptaskapp/view/TarefaViewHolder.java
+++ app/src/main/java/br/com/example/mobile/helptaskapp/view/TarefaViewHolder.java
@@ -1,4 +1,68 @@
 package br.com.example.mobile.helptaskapp.view;
 
-public class TarefaViewHolder {
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import br.com.example.mobile.helptaskapp.R;
+
+public class TarefaViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
+
+    public final TextView titulo;
+    public final TextView artista;
+    private Long tarefaId;
+    public final TarefaAdapter adapter;
+
+    public TarefaViewHolder(final View view, final TarefaAdapter adapter){
+        super(view);
+        this.adapter = adapter;
+
+        view.setOnClickListener(this);
+        view.setOnClickListener(this);
+
+        titulo = view.findViewById(R.id.tvTitulo);
+        artista = view.findViewById(R.id.tvArtista);
+    }
+
+    public void preencher(Musica musica){
+        musicaId = musica.getId();
+        titulo.setText(musica.getTitulo());
+        artista.setText(musica.getArtista());
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(view.getContext(), musicaId.toString(), Toast.LENGTH_SHORT).show();
+        Log.d("Musica selecionada", musicaId.toString());
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+        popupMenu.getMenuInflater().inflate(R.menu.musica_options, popupMenu.getMenu());
+
+        final Activity context = (Activity)view.getContext();
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener(){
+            public boolean onMenuItemClick(MenuItem item){
+                switch(item.getItemId()){
+
+                    case R.id.menuMusEditar:
+                        break;
+
+                    case R.id.menuMusDeletar:
+                        break;
+                }
+                return true;
+            }
+        });
+        popupMenu.show();
+        return false;
+
+    }
 }
