46b0579e173242bb9aad0b420f0f587d8f7ee374
vteodoro <viviteodoro19@gmail.com>
2018-05-10 06:21:23 -0300

    Editar MainActivity e activity_main

--- app/src/main/java/br/com/example/mobile/helptaskapp/view/MainActivity.java
+++ app/src/main/java/br/com/example/mobile/helptaskapp/view/MainActivity.java
@@ -3,19 +3,39 @@ package br.com.example.mobile.helptaskapp.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toolbar;
 
+import java.util.List;
+
 import br.com.example.mobile.helptaskapp.R;
+import br.com.example.mobile.helptaskapp.model.Tarefa;
+import br.com.example.mobile.helptaskapp.model.TarefaDAO;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TarefaDAO dao = TarefaDAO.manager;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Tarefa> tarefas = dao.getLista();
+
+        recyclerView = findViewById(R.id.rvTarefas);
+
+        recyclerView.setAdapter(new TarefaAdapter(tarefas, this));
+
+        RecyclerView.LayoutManager layout = new GridLayoutManager(this, 2);
+
+        recyclerView.setLayoutManager(layout);
+
     }
 
     @Override
